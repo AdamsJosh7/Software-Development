83b728dbb26f521ea8bb550977e1df0715720b32
AdamsJosh7 <adams.jos0648@stu.stech.edu>
2025-02-18 13:47:12 -0700

    Add files via upload

---
+++ Chapter 9/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+    public double width, height;
+
+    //constructor
+    public Rectangle(double w, double h) {
+
+        width = w;
+        height = h;
+    }
+}
---
+++ Chapter 9/testRectangle.java
@@ -0,0 +1,10 @@
+class testRectangle {
+	public static void main(String[] args) {
+		Rectangle rectangle1 = new Rectangle(4,40);
+		Rectangle rectangle2 = new Rectangle(3.5,35.9);
+		
+		double getArea() {
+			return w * h;
+		}
+	}
+}
