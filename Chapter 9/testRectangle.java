c141c7b2d523503bed93a333a77f963770cad640
AdamsJosh7 <adams.jos0648@stu.stech.edu>
2025-03-10 13:40:19 -0600

    Update testRectangle.java

--- Chapter 9/testRectangle.java
+++ Chapter 9/testRectangle.java
@@ -3,8 +3,23 @@ class testRectangle {
 		Rectangle rectangle1 = new Rectangle(4,40);
 		Rectangle rectangle2 = new Rectangle(3.5,35.9);
 		
-		double getArea() {
+		double getArea;
+		double getPerimeter;
+		
+		System.out.println("The Width of rectangle 1 is: ");
+		System.out.println("The Height of rectangle 1 is: ");
+		System.out.println("The Area of rectangle 1 is: ");
+		System.out.println("The Perimeter of rectangle 1 is: ");
+		System.out.println("The Width of rectangle 2 is: ");
+		System.out.println("The Height of rectangle 2 is: ");
+		System.out.println("The Area of rectangle 2 is: ");
+		System.out.println("The Perimeter of rectangle 2 is: ");
+		
+	}
+		double getArea(double w, double h) {
 			return w * h;
 		}
-	}
+		double getPerimeter(double w, double h) {
+			return (w * 2) + (h * 2);
+		}
 }
