416fb5272df9e0bb1770bccd00e3e22d9990b823
AdamsJosh7 <adams.jos0648@stu.stech.edu>
2025-03-19 13:35:29 -0600

    Update Rectangle.java

--- Chapter 9/Rectangle.java
+++ Chapter 9/Rectangle.java
@@ -3,8 +3,13 @@ public class Rectangle {
 
     //constructor
     public Rectangle(double w, double h) {
-
         width = w;
         height = h;
+    }
+    double getArea(double w, double h) {
+        return w * h;
+    }
+    double getPerimeter(double w, double h) {
+        return (w * 2) + (h * 2);
     }
 }
